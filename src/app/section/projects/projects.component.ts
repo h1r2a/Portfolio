8dd911c8ac6d7d0928bed546444e0f32c167da27
h1r2a <andriamihajaharena52@gmail.com>
2024-12-12 19:26:59 +0300

    some changes

--- src/app/section/projects/projects.component.ts
+++ src/app/section/projects/projects.component.ts
@@ -1,9 +1,9 @@
 import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGithub, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -22,7 +22,7 @@ export class ProjectsComponent implements OnInit {
   projects = [
     {
       name: 'KarVidy',
-      description: 'A web application that predict a car price based on ML model,made with reactjs and python ',
+      description: 'KarVidy is a web app that predicts car prices using machine learning. By analyzing factors like make, model, age, and mileage, it provides accurate price estimations. Built with ReactJS, Python, and FastAPI, it uses a trained model to offer valuable insights, helping users make informed decisions when buying or selling a car.',
       images: [
         'assets/karvidy-1.png',
         'assets/karvidy-2.png'
@@ -30,28 +30,26 @@ export class ProjectsComponent implements OnInit {
     },
     {
       name: 'TongaZa',
-      description: 'A smart attendance  based on facial recognition, made with reactjs and python',
+      description: 'TongaZa is an innovative attendance system powered by facial recognition technology. Developed with ReactJS and Python, it automates attendance tracking, offering a secure, contactless solution for schools and businesses. It ensures accurate identification and streamlines attendance management with a user-friendly interface.',
       images: [
         'assets/tongaza-1.png',
         'assets/tongaza-2.png'
-
       ],
     },
     {
       name: 'Direct Deal',
-      description: 'Point of sale app, made with MERN stack, cover all the main feature for modern POS app',
+      description: 'Direct Deal is a POS system built with the MERN stack. It integrates sales management, inventory tracking, and user management. Businesses can process transactions seamlessly, track inventory in real-time, and manage user roles securely, helping optimize sales and operations with an intuitive interface.',
       images: [
         'assets/d-deal.png'
       ],
     },
     {
       name: 'Business-Training',
-      description: 'Mobile app, that allow users to subscribe to courses that interest them ',
+      description: 'Business-Training is a mobile app developed with React Native, offering users a clean interface for accessing professional courses. Users can browse, subscribe to courses, and view schedules. With a focus on usability, it provides a simple solution for skill development without relying on a complex database system.',
       images: [
         'assets/business-training.png'
       ],
     },
-
   ];
 
   ngOnInit() {
@@ -61,6 +59,6 @@ export class ProjectsComponent implements OnInit {
       this.projects.forEach((project, index) => {
         this.currentImageIndex[index] = (this.currentImageIndex[index] + 1) % project.images.length;
       });
-    }, 2000); // 2000 ms = 2 secondes
+    }, 2000); // 2000 ms = 2 seconds
   }
 }
